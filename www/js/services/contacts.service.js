b2aee43a023c1009922c28f664b12185b41dc10d
Todd Halfpenny <todd@mobilecaddy.net>
2015-11-23 16:03:50 +0000

    Added a contact detail page

--- www/js/services/contacts.service.js
+++ www/js/services/contacts.service.js
@@ -20,19 +20,27 @@
 	  };
 
 	  /**
-	   * @description Gets the list of contacts for a particular account. Note we
-	   * are using underscore to filer these but we could just as easily use a
+	   * @description Gets the list of contacts for a particular account, or gets
+     * a single contact's details.
+     * Note we are using underscore to filer these but we could just as easily use a
 	   * smartSql query (see http://developer.mobilecaddy.net/docs/api/ for details)
 	   * @param  {string} accountId The Id of the account we are interested in.
 	   * @return {promise}	resolves to a list of contact records (or an error)
 	   */
-	  function get(accountId){
+	  function get(accountId, contactId){
       return new Promise(function(resolve, reject) {
         devUtils.readRecords('Contact__ap', []).then(function(resObject) {
         	// we have our contacts, now use underscorejs to get just the ones
         	// we want.
           var contacts = [];
-          contacts = _.where(resObject.records, {'AccountId': accountId });
+          if (typeof(contactId) != "undefined") {
+           contact = _.where(resObject.records, {'Id': contactId });
+           resolve(contact);
+          } else if (typeof(accountId) != "undefined") {
+           contacts = _.where(resObject.records, {'AccountId': accountId });
+          } else {
+            contacts = resObject.records;
+          }
           resolve(contacts);
         }).catch(function(resObject){
           reject(resObject);
